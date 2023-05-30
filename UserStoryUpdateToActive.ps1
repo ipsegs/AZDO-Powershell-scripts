a8d1f8b7313dd3f488b3515e9cad8371b6d7e6d6
Segun Adebayo <adebayosegun174@gmail.com>
2023-05-30 21:56:21 +0100

    Update UserStoryUpdateToActive.ps1

--- UserStoryUpdateToActive.ps1
+++ UserStoryUpdateToActive.ps1
@@ -1,22 +1,12 @@
-$organization = "organization-name"
+$organization = "org-name"
 $project = "project-name"
-$pat = "personal access token"
+$pat = "personal-access-token"
+$desired-state = "Active"
 
-$header = @{
-    Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$pat"))
-}
+#Authentication
+$header = $headers = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($pat)")) }
 
 $wiqlUri = "https://dev.azure.com/$organization/$project/_apis/wit/wiql?api-version=6.0"
-
-$parentQuery = @"
-SELECT [System.Id], [System.Title], [System.WorkItemType], [System.State], [System.ChangedDate]
-FROM workitems 
-WHERE [System.TeamProject] = '$project'
-AND [System.WorkItemType] = 'User Story' 
-AND [System.State] = 'Backlog'
-AND [System.State] <> 'Removed'
-ORDER BY [System.ChangedDate] DESC
-"@
 
 $childTasksQuery = @"
 SELECT [System.Id], [System.Title], [System.State], [System.IterationPath]
@@ -28,39 +18,25 @@ ORDER BY [Microsoft.VSTS.Common.Priority], [System.CreatedDate] DESC
 MODE (Recursive)
 "@
 
-$parentQueryBody = @{
-    query = $parentQuery
+$childTasksQueryBody = @{
+    query = $childTasksQuery
 }
 
-$response = Invoke-RestMethod -Uri $wiqlUri -Method Post -ContentType "application/json" -Headers $header -Body (ConvertTo-Json -InputObject $parentQueryBody)
+$response = Invoke-RestMethod -Uri $wiqlUri -Method Post -ContentType "application/json" -Headers $header -Body (ConvertTo-Json -InputObject $childTasksQueryBody)
 
-foreach ($parentWorkItem in $response.workItems) {
-    $childTasksQueryBody = @{
-        query = $childTasksQuery
-    }
+foreach ($childTaskRelation in $response.workItemRelations) {
+    $childTaskUrl = $childTaskRelation.target.url
+    $childTaskState = (Invoke-RestMethod -Uri $childTaskUrl -Method GET -Headers $header).fields.'System.State'
 
-    $childTasksResponse = Invoke-RestMethod -Uri $wiqlUri -Method POST -ContentType "application/json" -Headers $header -Body (ConvertTo-Json -InputObject $childTasksQueryBody)
-
-    $hasActiveChildTask = $childTasksResponse.workItemRelations | Where-Object {
-        $childTaskId = $_.target.id
-        $childTaskUrl = $_.target.url
-        $childTaskState = (Invoke-RestMethod -Uri $childTaskUrl -Method GET -Headers $header).fields.'System.State'
-        $childTaskState -eq 'Active'
-    }
-
-    foreach ($activeChildTask in $hasActiveChildTask) {
-        $parentUserStoryUrl = $activeChildTask.source.url
-        # Retrieve the source URL
-        $sourceUrl = $activeChildTask.source.url
-
+    if ($childTaskState -eq 'Active') {
+        $parentUserStoryUrl = $childTaskRelation.source.url
+        $sourceUrl = $childTaskRelation.source.url
         $parentWorkItemUrl = $parentUserStoryUrl + "?api-version=7.0"
-        $parentWorkItemUrl | ConvertTo-Json
-        
-        $updateBody = "[{"op": "add","path": "/fields/System.State","value": "Active"}]"
+        $updateBody = '[{"op": "add", "path": "/fields/System.State", "value": "$desired-state"}]'
 
         Invoke-RestMethod -Uri $parentWorkItemUrl -Method Patch -ContentType "application/json-patch+json" -Headers $header -Body $updateBody
 
-        Write-Output "Parent User Story with URL $parentUserStoryUrl has been updated to Active."
+        Write-Output "Parent User Story with URL $parentUserStoryUrl has been updated to $desired-state."
         Write-Output "Source URL: $sourceUrl"
-        }
+    }
 }
